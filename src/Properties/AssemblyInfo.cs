eb9daa384f34e29c04297dbcae210bbb7808e18c
Weilbyte <me@weilbyte.dev>
2025-07-12 13:54:30 +0200

    Update for 1.6

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weilbyte")]
 [assembly: AssemblyProduct("SnapOut")]
-[assembly: AssemblyCopyright("Copyright Weilbyte © 2024")]
+[assembly: AssemblyCopyright("Copyright Weilbyte © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d7d21b4a-1da7-41d8-b202-c58ca8fa62aa")]
 
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
